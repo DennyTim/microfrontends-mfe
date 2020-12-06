34c799ca10fc04571f6eca24063a069b10ef9d0c
dety0819 <luckystudydanit@gmail.com>
2020-12-06 14:42:08 +0200

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import {
+    createGenerateClassName,
+    StylesProvider
+} from '@material-ui/core/styles';
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
-import { BrowserRouter } from "react-router-dom";
+
+const generatedClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header/>
-                <MarketingApp/>
-            </div>
+            <StylesProvider generateClassName={ generatedClassName }>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,14 +4,18 @@ import {
     Route,
     BrowserRouter
 } from "react-router-dom";
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from "./components/Pricing";
 
+const generatedClassName = createGenerateClassName({
+   productionPrefix: 'ma'
+});
+
 export default () => {
     return (<div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generatedClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={ Pricing }/>
